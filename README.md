5b22181d987c79791b9b57ef6924e2c5d2ff3136
Martín <magadiflo@gmail.com>
2022-09-02 11:24:24 -0500

    Inicio del proyecto - Spring Boot REST API Request Validation Examples

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Spring Boot REST API Request Validation Examples](https://www.youtube.com/watch?v=eX8Wr5MeEnw)
+Tomado del canal de **Code Java**
+
---
+++ src/main/java/com/magadiflo/app/SbRestApiRequestValidationApplication.java
@@ -0,0 +1,13 @@
+package com.magadiflo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestApiRequestValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRestApiRequestValidationApplication.class, args);
+	}
+
+}
